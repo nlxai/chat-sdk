b42c3a17d0a160de55406fba4097e8cad356eef4
romanistnlx <raman.smuniou@nlx.ai>
2022-12-27 13:46:52 +0400

    chore: remove dev react

--- packages/widget/bin/build.js
+++ packages/widget/bin/build.js
@@ -1,12 +1,15 @@
 const browserify = require("browserify");
+const commonShake = require('common-shakeify');
 const fs = require("fs");
 
 fs.mkdir("lib/umd", () => {
+  process.env.NODE_ENV = 'production';
   browserify({
     standalone: "chat",
   })
     .add("src/index.tsx")
     .plugin("tsify")
+    .plugin(commonShake)
     .transform("uglifyify", { global: true })
     .bundle()
     .on("error", (error) => {
