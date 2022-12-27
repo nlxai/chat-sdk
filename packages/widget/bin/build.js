054509dcbfb9bcc114d8febaaa297b93c5b71781
Peter Szerzo <szerzo.peter@gmail.com>
2022-12-27 11:40:26 +0100

    Adjust widget build script

--- packages/widget/bin/build.js
+++ packages/widget/bin/build.js
@@ -1,15 +1,16 @@
 const browserify = require("browserify");
-const commonShake = require('common-shakeify');
+const commonShake = require("common-shakeify");
 const fs = require("fs");
 
 fs.mkdir("lib/umd", () => {
-  process.env.NODE_ENV = 'production';
+  process.env.NODE_ENV = "production";
   browserify({
     standalone: "chat",
   })
     .add("src/index.tsx")
     .plugin("tsify")
-    .plugin(commonShake)
+    // Remove temporarily until we get it to work properly
+    // .plugin(commonShake)
     .transform("uglifyify", { global: true })
     .bundle()
     .on("error", (error) => {
