4d94ddbb96ca260c376a86c8ba64dd5e344de1eb
Peter Szerzo <szerzo.peter@gmail.com>
2023-09-19 09:12:59 +0200

    Update website version

--- packages/widget/src/website/index.tsx
+++ packages/widget/src/website/index.tsx
@@ -268,7 +268,7 @@ const App = () => {
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    <script defer src="https://unpkg.com/@nlxchat/widget@2.0.0-alpha.1/lib/umd/index.js"></script>
+    <script defer src="https://unpkg.com/@nlxchat/widget@2.0.0-alpha.6/lib/umd/index.js"></script>
     <script>
       window.addEventListener("DOMContentLoaded", () => {
         let welcomeIntentSent = false;
